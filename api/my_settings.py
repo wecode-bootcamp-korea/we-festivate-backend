3b14c83a0dc164dcce94f1e255ea8feb8b342813
drmgrammer <drmgrammer@gmail.com>
2019-07-02 09:52:51 +0900

    personal settings file 추가, setting.py에 db 수정, DB name = wef

---
+++ api/my_settings.py
@@ -0,0 +1,13 @@
+dbSettings =  {
+        'default':
+        {
+            'ENGINE': 'mysql.connector.django',
+            'NAME': 'wef',
+            'USER': 'root',
+            'PASSWORD': 'EHWJSGKWK1!',
+            'HOST': 'localhost',
+            'PORT':'3306',
+            # 'default-character-set': 'utf8'
+        }      
+}
+
