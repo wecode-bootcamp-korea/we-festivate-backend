2879e26043d5fe97fc46f794a206ea0265807c40
drmgrammer <drmgrammer@gmail.com>
2019-07-03 11:21:47 +0900

    user model 에 대한 migrate 진행
    세부 sql 문:
     python manage.py sqlmigrate user 0001
    System check identified some issues:
    
    WARNINGS:
    ?: (urls.W002) Your URL pattern '/' has a route beginning with a '/'. Remove this slash as it is unnecessary. If this pattern is targeted in an include(), ensure the include() pattern has a trailing '/'.
    BEGIN;
    --
    -- Create model User
    --
    CREATE TABLE `user` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `user_id` varchar(20) NOT NULL UNIQUE, `pw` varchar(100) NOT NULL, `name` varchar(100) NOT NULL, `email` varchar(100) NOT NULL UNIQUE, `profile` varchar(200) NOT NULL, `user_type` varchar(100) NOT NULL, `created_at` datetime(6) NOT NULL, `updated_at` datetime(6) NOT NULL);
    COMMIT;

--- api/user/models.py
+++ api/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class User(models.Model):
+    user_id = models.CharField(max_length=20, unique=True)
+    pw = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    profile = models.CharField(max_length=200)
+    user_type = models.CharField(max_length=100, default="00")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    class Meta:
+        db_table = "user"
+
