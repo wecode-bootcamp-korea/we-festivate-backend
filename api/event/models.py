7416341975c75505fa45bf5b6be1895eb597826c
drmgrammer <drmgrammer@gmail.com>
2019-07-03 13:25:35 +0900

    event model 추가 및 migrate 완료
    sqlmigrate 세목
    
    python manage.py sqlmigrate event 0001
    System check identified some issues:
    
    WARNINGS:
    ?: (urls.W002) Your URL pattern '/' has a route beginning with a '/'. Remove this slash as it is unnecessary. If this pattern is targeted in an include(), ensure the include() pattern has a trailing '/'.
    BEGIN;
    --
    -- Create model EventPost
    --
    CREATE TABLE `event_eventpost` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `title` varchar(50) NOT NULL, `place` varchar(50) NOT NULL, `date` varchar(20) NOT NULL, `stat_time` varchar(20) NOT NULL, `end_time` varchar(20) NOT NULL, `max_rsvp` integer NOT NULL, `current_rsvp` integer NOT NULL, `url` varchar(100) NOT NULL, `main_text` longtext NOT NULL, `create_at` datetime(6) NOT NULL, `updated_at` datetime(6) NOT NULL, `writer_id` integer NULL);
    ALTER TABLE `event_eventpost` ADD CONSTRAINT `event_eventpost_writer_id_e88d662f_fk_user_id` FOREIGN KEY (`writer_id`) REFERENCES `user` (`id`);
    COMMIT;

--- api/event/models.py
+++ api/event/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from  user.models  import User
 
-# Create your models here.
+class EventPost(models.Model):
+    title = models.CharField(max_length=50)
+    place = models.CharField(max_length=50)                    # 이벤트 진행 상세 위치  예) 8층 메인라운지
+    date = models.CharField(max_length=20, blank=True)         # 이벤트 진행날짜
+    stat_time = models.CharField(max_length=20, blank=True)    # 이벤트 시작시간
+    end_time = models.CharField(max_length=20, blank=True)     # 이벤트 종료시간
+    max_rsvp = models.IntegerField(blank=True)                 # 최대 신청 허용 인원
+    current_rsvp = models.IntegerField(default=0)              # 현재 신청 인원
+    url = models.URLField(max_length=100)                      # members.wework.com 에 올려져있는 이벤트 url 링크
+    main_text = models.TextField(max_length=2000 ,blank=True)  # 본문 내용
+    writer = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)  # User.models 파일 추가 필요
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
