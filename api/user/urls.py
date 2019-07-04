b371951311e6191d44c2e0c058826f24a470e341
seung-jae <lee@seung-jaeui-MacBookPro.local>
2019-07-04 16:38:08 +0900

    user/views.py UserView(회원가입) 코딩_v1
    user/views.py 회원가입 클라스 작성
    AccountView에서 UserView로 클라스명 변경
    
    migrate 하기 전

--- api/user/urls.py
+++ api/user/urls.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse, JsonResponse
-from django.views import View #why??
+from django.views import View 
 from django.urls import path, include
 from . import views
-from .views import testing
 
 urlpatterns = [
-    path('/', views.testing),
-]
+    path('/', UserView.as_view()),
+    ]
 
--- api/user/views.py
+++ api/user/views.py
@@ -1,5 +1,30 @@
-from django.http import HttpResponse, JsonResponse
-from django.views import View #why??
+import json
+import bcrypt
 
-def testing(request):
-    return HttpResponse("<h1>장고 넌 죽었어 우리한테 </h1>")
+from .models import User
+from django.views import View
+from django.http import JsonResponse, HttpResponse
+
+class UserView(View):
+    def post(self, request):
+        new_user_info = json.loads(request.body)
+
+        if User.objects.filter(name=new_user_info['name']).exists():
+            return JsonResponse({'message' : '이미존재하는 아이디입니다.'}, status=400)
+        else:
+            password = bytes(new_user_info["password"], "utf-8")
+            hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+            new_user_info = User(
+                    user_id = new_user_info['user_id'],
+                    user_password = hashed_password.decode("UTF-8"),
+                    name = new_user_info['name'],
+                    email = new_user_info['email'],
+                    profile = new_user_info['profile'],
+                    user_type = new_user_info['user_type'],
+                    member_building = new_user_info['member_building'],
+                    )
+            new_user_info.save()
+
+            return JsonResponse({'message' : '회원가입이 완료되었습니다.'}, (status = 200))
+
