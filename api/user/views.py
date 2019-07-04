160855be949c3f26f9e88350643f3206349021f2
seung-jae <lee@seung-jaeui-MacBookPro.local>
2019-07-04 22:02:36 +0900

    이메일이 같으면 이미 가입된 이메일입니다라는 메세지 출력
    가장 최근에 커밋한 데이터를 기준으로 httpie 테스트를 진행해보니
    500error가 출력되어 확인해보니 이메일이 같아서 인 것을 확인하여
    if문을 수정함

--- api/user/views.py
+++ api/user/views.py
@@ -10,7 +10,10 @@ class UserView(View):
     def post(self, request):
         new_user_info = json.loads(request.body)
 
-        if User.objects.filter(name=new_user_info['name']).exists():
+        if User.objects.filter(email=new_user_info['email']).exists():
+            return JsonResponse({'message' : '이미 가입된 이메일 주소입니다.'}, status=400)
+
+        if User.objects.filter(user_id=new_user_info['user_id']).exists():
             return JsonResponse({'message' : '이미존재하는 아이디입니다.'}, status=400)
         else:
             password = bytes(new_user_info["password"], "utf-8")
@@ -28,4 +31,15 @@ class UserView(View):
             new_user_info.save()
 
             return JsonResponse({'message' : '회원가입이 완료되었습니다.'}, status = 200)
+
+#class LoginView(View):
+ #   def post(self, request):
+  #      login_user_info = json.loads(request, body)
+
+   #     if User.objects.filter(name=
+        
+
+
+
+
 
