5b4bb377e0f284d878ab7eab11d5e4c717d852fa
drmgrammer <drmgrammer@gmail.com>
2019-07-13 19:54:18 +0900

    수정 요구사항 반영 및 서칭 기능 추가

--- api/event/urls.py
+++ api/event/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
-from .views import DetailView, AllView, PriorityView, NewestView, RsvpView, CommentView, NewEventView
+from .views import EventDetailView, AllView, PriorityView, NewestView, EventTitleSearchView, RsvpView, CommentView, NewEventView, LikeView
 
 urlpatterns = [
     path('/', views.event_test),
     path('/newest', NewestView.as_view()),
     path('/priority', PriorityView.as_view()),
     path('/all', AllView.as_view()),
-    path('/search', views.search),
-    path('/detail/<int:event_id>', DetailView.as_view()),
+    path('/title_search', EventTitleSearchView.as_view()),
+    path('/detail/<int:event_id>', EventDetailView.as_view()),
     path('/detail/<int:event_id>/rsvp', RsvpView.as_view()),
+    path('/detail/<int:event_id>/like', LikeView.as_view()),
     path('/detail/<int:event_id>/comment', CommentView.as_view()),
     path('/write', NewEventView.as_view()),
 ]
--- api/event/views.py
+++ api/event/views.py
@@ -1,4 +1,4 @@
-from .models.event_models import EventPost, EventRsvp, EventComment
+from .models.event_models import EventPost, EventRsvp, EventComment, EventLike
 from user.models import User, UserType
 from .models.building_models import Building
 from django.views import View
@@ -16,7 +16,7 @@ def event_test(request):
 class NewestView(View):
     def get(self, request):
         post_num = int(request.GET.get('num',POST_NUM))
-        event_list = list(EventPost.objects.order_by('created_at').select_related('building').values('id','title','photo_url','date','max_rsvp','current_rsvp','building__name')[:post_num])
+        event_list = list(EventPost.objects.order_by('-created_at').select_related('building').values('id','title','photo_url','date','max_rsvp','current_rsvp','building__name')[:post_num])
         return JsonResponse(event_list, safe=False)
 
 class PriorityView(View):
@@ -24,7 +24,14 @@ class PriorityView(View):
     def get(self, request):
         post_num = int(request.GET.get('num',POST_NUM))
         todayStr = str(DateFormat(datetime.now()).format("ymd"))
-        event_list = list(EventPost.objects.filter(date=todayStr).select_related('building').values('id','title','photo_url','date','max_rsvp','current_rsvp','building__name')[:post_num])
+        event_list = list(EventPost.objects.filter(date=todayStr).select_related('building').values(
+            'id',
+            'title',
+            'photo_url',
+            'date',
+            'max_rsvp',
+            'current_rsvp',
+            'building__name')[:post_num])
         return JsonResponse(event_list, safe=False)
 
 class AllView(View):
@@ -32,27 +39,56 @@ class AllView(View):
     def get(self,request):
         start_id = int(request.GET.get('start',0))
         end_id = int(request.GET.get('end',POST_NUM))
-        event_list = list(EventPost.objects.order_by('id').select_related('building').values('id','title','photo_url','date','max_rsvp','current_rsvp','building__name')[start_id:end_id])
+        event_list = list(EventPost.objects.order_by('-id').select_related('building').values(
+            'id',
+            'title',
+            'photo_url',
+            'date',
+            'max_rsvp',
+            'current_rsvp',
+            'building__name')[start_id:end_id])
         return JsonResponse(event_list, safe=False)
 
-def search(request):
-    return HttpResponse("<h1>검색 조건에 맞는 이벤트를 전달합니다! </h1>")
+class EventTitleSearchView(View):
 
-class DetailView(View):
+    def post(self,request):
+        query_string = json.loads(request.body)
+        # title_q = request.GET.get('title_q','')
+
+        if query_string["title_query"]:
+            title_query_data = EventPost.objects.values().filter(title__icontains=query_string["title_query"])
+        else:
+            title_query_data = EventPost.objects.values().all()
+
+        return JsonResponse(list(title_query_data), safe=False)
+
+class EventDetailView(View):
 
     def post(self, request, event_id):
         page_user = json.loads(request.body)
-        user1 = User.objects.get(user_id=page_user["user_id"])
-        print(user1.id)
+        if page_user["login_state"]:
+            user1 = User.objects.get(id=page_user["user_pk"])
 
-        if EventRsvp.objects.filter(user_id=user1).exists():
-            rsvp_result = True
-        else :
+            if EventRsvp.objects.filter(user_id=user1.id, event_id=event_id).exists():
+                rsvp_result = True
+            else :
+                rsvp_result = False
+
+            if EventLike.objects.filter(user_id=user1.id, event_id=event_id).exists():
+                like_result = True
+            else :
+                like_result = False
+
+        else:
             rsvp_result = False
+            like_result = False
 
         event = EventPost.objects.get(pk=event_id)
         building =Building.objects.get(pk=event.building_id)
-        event_comment1 = list(EventComment.objects.filter(event_id=event_id).prefetch_related('user').values())
+        event_comment = list(EventComment.objects.filter(event_id=event_id).prefetch_related('user').values(
+            'user__name',
+            'comment_text',
+            'created_at'))
 
         return JsonResponse ({
             "event_id": event.id,
@@ -77,28 +113,30 @@ class DetailView(View):
             "latitude": building.latitude,
             "longitude": building.longitude,
             "rsvp_result": rsvp_result,
-            "event_comment": event_comment1
+            "like_result": like_result,
+            "event_comment": event_comment
         }, safe=False)
 
 class RsvpView(View):
 
     def post(self, request, event_id):
         page_user = json.loads(request.body)
-        user1 = User.objects.get(user_id=page_user["user_id"])
+        user1 = User.objects.get(id=page_user["user_pk"])
         rsvp_event = EventPost.objects.get(id=event_id)
 
         if EventRsvp.objects.filter(user_id=user1.id, event_id=event_id).exists(): #user_id는 integer 값으로 user테이블의 pk와 동일
             rsvp_result = False
-            rsvp_message = '이미 신청한 이벤트 입니다.'
+            rsvp_message = 'rsvp_reserved'
         elif rsvp_event.current_rsvp >= rsvp_event.max_rsvp: #추후 개선 필요
             rsvp_result = False
-            rsvp_message = '이벤트 신청이 완료 되었습니다. 댓글을 남겨주시면 대기자 명단 처리 하겠습니다.'
+            rsvp_message = 'rsvp_overflow'
         else :
             rsvp_event.current_rsvp += 1
             rsvp_event.save()
-            EventRsvp.objects.create(event_id=event_id, user_id=user1.id)
+            new_rsvp = EventRsvp.objects.create(event_id=event_id, user_id=user1.id)
+            new_rsvp.save()
             rsvp_result = True
-            rsvp_message = '이벤트 신청이 정상 처리되었습니다.'
+            rsvp_message = 'rsvp_success'
 
         return JsonResponse ({
             "current_rsvp": rsvp_event.current_rsvp,
@@ -106,19 +144,53 @@ class RsvpView(View):
             "rsvp_message": rsvp_message
         }, safe=False)
 
+class LikeView(View):
+
+    def post(self, request, event_id):
+        page_user = json.loads(request.body)
+        user1 = User.objects.get(id=page_user["user_pk"])
+        like_event = EventPost.objects.get(id=event_id)
+
+        if EventLike.objects.filter(
+            user_id=user1.id,
+            event_id=event_id).exists():
+            #user_id는 integer 값으로 user테이블의 pk와 동일
+            like_result = False
+            like_message = 'like_fail'
+        else :
+            like_event.like_num += 1
+            like_event.save()
+            new_like = EventLike.objects.create(event_id=event_id, user_id=user1.id)
+            new_like.save()
+            like_result = True
+            like_message = 'like_success'
+
+        return JsonResponse ({
+            "like_num": like_event.current_rsvp,
+            "like_result": like_result,
+            "like_message": like_message
+        }, safe=False)
+
 class CommentView(View):
 
     def post(self, request, event_id):
         comment_data = json.loads(request.body)
-        user1 = User.objects.get(user_id=comment_data["user_id"])
-        EventComment.objects.create(event_id=event_id, user_id=user1.user_id, comment_text=comment_data["comment_text"])
+        user1 = User.objects.get(id=comment_data["user_pk"])
+
+        #새로운 댓글 로우데이터 작성
+        new_comment = EventComment.objects.create(
+            event_id=event_id, user_id=user1.id,
+            comment_text=comment_data["comment_text"],
+            created_at=comment_data["created_at"])
+        new_comment.save()
         comment_result = True
-        comment_message = "님, 댓글이 정상적으로 등록되었습니다."
+        result_message = "comment_success"
 
         return JsonResponse ({
-            "user_id": user1.user_id,
-            "comment_result": True,
-            "comment_message": comment_message
+            "user_pk": user1.id,
+            "user_name": user1.name,
+            "message": result_message,
+            "comment_result": True
         }, safe=False)
 
 class NewEventView(View):
@@ -126,11 +198,16 @@ class NewEventView(View):
     def post(self, request):
 
         new_event = json.loads(request.body)
-        user1 = User.objects.get(user_id=new_event["user_id"])
+        user1 = User.objects.get(id=new_event['user_pk'])
+        event_building = Building.objects.get(id=new_event['building_id'])
 
-        EventPost.objects.create(title=new_event["title"], building=new_event["building_id"], place=new_event["place"],date=new_event["date"], start_time=new_event["start_time"], end_time=new_event["end_time"],max_rsvp=new_event["max_rsvp"],event_page_url=new_event["event_page_url"],main_text=new_event["main_text"],photo_url=new_event["photo_url"],event_host=user1.id)
-        message="이벤트가 정상적으로 등록되었습니다."
+        new_event_data = EventPost.objects.create(title=new_event["title"], building=event_building, place=new_event["place"],date=new_event["date"], start_time=new_event["start_time"], end_time=new_event["end_time"],max_rsvp=new_event["max_rsvp"],main_text=new_event["main_text"],event_host=user1, photo_url=new_event["photo_url"]) 
+        #추후 추가 필요  event_page_url=new_event["event_page_url"]
+        new_event_data.save()
+        message="event_register_success"
 
+        EventPost.objects.filter
         return JsonResponse({
-            "result_message": message
+            "result_message": message,
+            "event_id": new_event_data.id
         })
