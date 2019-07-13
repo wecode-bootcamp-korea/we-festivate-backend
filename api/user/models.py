7eae20be6f5508dddf63ea60d6ce9c781dcfaa84
seung-jae <suengjae12@naver.com>
2019-07-13 20:30:19 +0900

    소셜로그인 전 구현 후 주석처리 해두었던 models.py User 클래스 삭제

--- api/user/models.py
+++ api/user/models.py
@@ -6,20 +6,6 @@ class UserType(models.Model):
     class Meta:
         db_table = "user_type"
 
-# class User(models.Model):
-#     user_id    = models.CharField(max_length=20, unique=True)
-#     password   = models.CharField(max_length=100)
-#     name       = models.CharField(max_length=100)
-#     email      = models.EmailField(max_length=100, unique=True)
-#     profile    = models.CharField(max_length=200)
-#     user_type  = models.ForeignKey(UserType, blank=True, null=True, on_delete=models.CASCADE, default=1)
-#     # 1 : type = non_wework_member
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-    # class Meta:
-    #     db_table = "user"
-
 class SocialPlatform(models.Model):
     platform = models.CharField(max_length=20, default=0)
 
@@ -34,7 +20,7 @@ class User(models.Model):
     email           = models.EmailField(max_length=100, unique=True, null=True)
     profile         = models.CharField(max_length=200)
     user_type       = models.ForeignKey(UserType, blank=True, null=True, on_delete=models.CASCADE, default=1)
-    # 1 : type      = non_wework_member
+    # user_type= 1  = non_wework_member
     social          = models.ForeignKey(SocialPlatform, on_delete=models.CASCADE, max_length=20, blank=True, default=1)
     social_login_id = models.CharField(max_length=50, blank=True)
     created_at      = models.DateTimeField(auto_now_add=True)
